c815d6692880d81b74bf3683f19c8783ffd4f28e
kirico <kirico1027@icloud.com>
2024-03-05 14:51:55 +0900

    セレクトボックス修正

--- WordPressTheme/functions.php
+++ WordPressTheme/functions.php
@@ -200,3 +200,43 @@ function remove_wysiwyg()
   remove_post_type_support('campaign', 'editor');
 }
 add_action('init', 'remove_wysiwyg');
+
+
+//セレクトボックスにcampaignの投稿のタイトルを反映させる
+function filter_wpcf7_form_tag($scanned_tag, $replace)
+{
+  if (!empty($scanned_tag)) {
+
+    //nameで判別
+    if ($scanned_tag['name'] == 'campaign-select') {
+
+      //カスタム投稿タイプの取得
+      global $post;
+      $args = array(
+        'posts_per_page' => -1,
+        'post_type' => 'campaign',
+        'order' => 'DESC',
+      );
+      $customPosts = get_posts($args);
+      if ($customPosts) {
+        foreach ($customPosts as $post) {
+          setup_postdata($post);
+
+          $title = get_the_title();
+
+          //$scanned_tagに情報を追加
+          $scanned_tag['values'][] = $title;
+          $scanned_tag['labels'][] = $title;
+        }
+      }
+      wp_reset_postdata();
+    }
+  }
+  return $scanned_tag;
+};
+add_filter( 'wpcf7_form_tag', 'filter_wpcf7_form_tag', 11, 2 );
+add_filter('protected_title_format', 'remove_protected');
+function remove_protected($title)
+{
+  return '%s';
+}
